aa6280266236b4b613f377d554f86e593c358037
longwei <longwei.su@gmail.com>
2015-03-11 22:09:03 -0400

    add user

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ __author__ = 'longwei'
 from flask import Flask
 from flask import render_template, request
 from flask_bootstrap import Bootstrap
+from flask_security import Security, SQLAlchemyUserDatastore, \
+    UserMixin, RoleMixin, login_required
 import os
 
 #forms
@@ -13,16 +15,44 @@ from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 
 class IdeaForm(Form):
-    idea_name = StringField('idea', validators=[DataRequired()])
-    submit_button = SubmitField('Add a idea')
+    idea_name = StringField('Idea', validators=[DataRequired()])
+    submit_button = SubmitField('Add idea')
 
 
 app = Flask(__name__)
 # in a real app, these should be configured through Flask-Appconfig
 app.config['SECRET_KEY'] = 'devkey'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SECURITY_REGISTERABLE'] = True
+app.config['SECURITY_SEND_REGISTER_EMAIL'] = False
+app.config['SECURITY_SEND_PASSWORD_CHANGE_EMAIL'] = False
+app.config['SECURITY_SEND_PASSWORD_RESET_NOTICE_EMAIL'] = False
 
+#create db connection
 db = SQLAlchemy(app)
+
+# Define models
+roles_users = db.Table('roles_users',
+        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
+# Setup Flask-Security
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
 
 
 class Idea(db.Model):
@@ -46,7 +76,8 @@ def index():
         idea = Idea(form.idea_name.data)
         db.session.add(idea)
         db.session.commit()
-    list_of_ideas = Idea.query
+    list_of_ideas = Idea.query.all()
+    #current_user is magically passed in
     return render_template("index.html", form = form, list_of_ideas= list_of_ideas)
 
 
